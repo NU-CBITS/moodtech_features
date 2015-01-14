92b0deded8112e991863c43e298238ce6bd4220b
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-01-14 14:05:58 -0600

    updating researcher groups spec

--- spec/features/user/researcher_groups_spec.rb
+++ spec/features/user/researcher_groups_spec.rb
@@ -13,22 +13,22 @@ describe "Research, Groups", :type => :feature, :sauce => false do
 
   before(:each) do
     Capybara.default_driver = :selenium
-  end
-
-#tests
-
-#Testing creating a group
-  it "- create a group" do
     visit ENV['Base_URL']+ '/users/sign_in'
     within("#new_user") do
-      fill_in 'user_email', :with => ENV['User_Email']
-      fill_in 'user_password', :with => ENV['User_Password']
+      fill_in 'user_email', :with => ENV['Researcher_Email']
+      fill_in 'user_password', :with => ENV['Researcher_Password']
     end
     click_on 'Sign in'
     expect(page).to have_content 'Signed in successfully'
     expect(page).to have_content 'CSV Reports'
     click_on 'Groups'
     expect(page).to have_content 'Listing Groups'
+  end
+
+#tests
+
+#Testing creating a group
+  it "- create a group" do
     click_on 'New'
     expect(page).to have_content 'New Group'
     fill_in 'group_title', :with => 'Testing Group'
@@ -40,46 +40,26 @@ describe "Research, Groups", :type => :feature, :sauce => false do
 
   #Testing updating a group
   it "- update a group" do
-    visit ENV['Base_URL']+ '/users/sign_in'
-    within("#new_user") do
-      fill_in 'user_email', :with => ENV['User_Email']
-      fill_in 'user_password', :with => ENV['User_Password']
-    end
-    click_on 'Sign in'
-    expect(page).to have_content 'Signed in successfully'
-    expect(page).to have_content 'CSV Reports'
-    click_on 'Groups'
-    expect(page).to have_content 'Listing Groups'
-    click_on 'fake'
-    expect(page).to have_content 'Title: fake'
+    click_on 'Group 1'
+    expect(page).to have_content 'Title: Group 1'
     click_on 'Edit'
     expect(page).to have_content 'Editing Group'
-    fill_in 'group_title', :with => 'Updated fake'
+    fill_in 'group_title', :with => 'Updated Group 1'
     click_on 'Update'
     expect(page).to have_content 'Group was successfully updated.'
-    expect(page).to have_content 'Title: Updated fake'
+    expect(page).to have_content 'Title: Updated Group 1'
     click_on 'Edit'
     expect(page).to have_content 'Editing Group'
-    fill_in 'group_title', :with => 'fake'
+    fill_in 'group_title', :with => 'Group 1'
     click_on 'Update'
     expect(page).to have_content 'Group was successfully updated.'
-    expect(page).to have_content 'Title: fake'
+    expect(page).to have_content 'Title: Group 1'
   end
 
   #Testing adding/removing a moderator from a group
   it "- update moderator" do
-    visit ENV['Base_URL']+ '/users/sign_in'
-    within("#new_user") do
-      fill_in 'user_email', :with => ENV['User_Email']
-      fill_in 'user_password', :with => ENV['User_Password']
-    end
-    click_on 'Sign in'
-    expect(page).to have_content 'Signed in successfully'
-    expect(page).to have_content 'CSV Reports'
-    click_on 'Groups'
-    expect(page).to have_content 'Listing Groups'
-    click_on 'fake'
-    expect(page).to have_content 'Title: fake'
+    click_on 'Group 1'
+    expect(page).to have_content 'Title: Group 1'
     click_on 'Edit'
     select ENV['Clinician_Email'], :from => 'group_moderator_id'
     click_on 'Update'
@@ -92,16 +72,6 @@ describe "Research, Groups", :type => :feature, :sauce => false do
 
   #Testing destroying a group
   it "- destroy a group" do
-    visit ENV['Base_URL']+ '/users/sign_in'
-    within("#new_user") do
-      fill_in 'user_email', :with => ENV['User_Email']
-      fill_in 'user_password', :with => ENV['User_Password']
-    end
-    click_on 'Sign in'
-    expect(page).to have_content 'Signed in successfully'
-    expect(page).to have_content 'CSV Reports'
-    click_on 'Groups'
-    expect(page).to have_content 'Listing Groups'
     click_on 'Testing Group'
     expect(page).to have_content 'Title: Testing Group'
     click_on 'Destroy'
@@ -112,25 +82,15 @@ describe "Research, Groups", :type => :feature, :sauce => false do
 
   #Testing managing tasks
   it "- manage tasks within a group" do
-    visit ENV['Base_URL']+ '/users/sign_in'
-    within("#new_user") do
-      fill_in 'user_email', :with => ENV['User_Email']
-      fill_in 'user_password', :with => ENV['User_Password']
-    end
-    click_on 'Sign in'
-    expect(page).to have_content 'Signed in successfully'
-    expect(page).to have_content 'CSV Reports'
-    click_on 'Groups'
-    expect(page).to have_content 'Listing Groups'
-    click_on 'fun'
-    expect(page).to have_content 'Title: fun'
+    click_on 'Group 1'
+    expect(page).to have_content 'Title: Group 1'
     click_on 'Manage Tasks'
     expect(page).to have_content 'Recurring termination day (if applicable)'
-    select 'DO: #1 Awareness', :from => 'task_bit_core_content_module_id'
+    select 'LEARN: Do - Planning Slideshow 3 of 4', :from => 'task_bit_core_content_module_id'
     fill_in 'task_release_day', :with => '1'
     click_on 'Assign'
     expect(page).to have_content 'Task assigned.'
-    find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/table/tbody/tr[2]/td[6]/a').click
+    find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/table/tbody/tr[23]/td[6]/a').click
     page.accept_alert 'Are you sure?'
     within '#tasks' do
       expect(page).to_not have_content 'DO: #1 Awareness'
