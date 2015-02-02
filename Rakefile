f034566c1ff0062d9bf22299b782f18523fae2df
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-02-02 15:21:37 -0600

    Revert "updates to satisfy rubocop"
    This reverts commit da6c27d4f2a7e1d3e54db201696de6be41af9045.

--- Rakefile
+++ Rakefile
@@ -1,37 +1,39 @@
-# filename: Rakefile
+#filename: Rakefile
 
-# load development version of think_feel_do_so locally
+#load development version of think_feel_do_so locally
 
-desc 'Setting database and starting think_feel_do_so for testing locally'
+desc "Setting database and starting think_feel_do_so for testing locally"
 
 task :load_tfdso_local do
   Dir.chdir('/Users/Chris/Work/think_feel_do_so') do
-    system('rake db:drop db:create db:migrate')
-    system('rake seed:with_fixtures')
-    system('rails s')
+    system( "rake db:drop db:create db:migrate" )
+    system( "rake seed:with_fixtures" )
+    system( "rails s" )
   end
 end
 
-# load development version of think_feel_do_so on staging
 
-desc 'Setting test database for testing on staging'
+#load development version of think_feel_do_so on staging
+
+desc "Setting test database for testing on staging"
 
 task :load_tfdso_test do
-  system('export Base_URL=https://moodtech-staging.cbits.northwestern.edu')
+  system( "export Base_URL=https://moodtech-staging.cbits.northwestern.edu" )
   Dir.chdir('User/Chris/Work/think_feel_do_so') do
-    system('cap staging deploy:use_test_db')
-    system('cap staging deploy:clean_db')
-    system('cap staging deploy:migrate_db')
-    system('cap staging deploy:seed_db')
+    system( "cap staging deploy:use_test_db" )
+    system( "cap staging deploy:clean_db" )
+    system( "cap staging deploy:migrate_db" )
+    system( "cap staging deploy:seed_db" )
   end
 end
 
-# load staging version of think_feel_do_so on staging
 
-desc 'Returning staging database on staging'
+#load staging version of think_feel_do_so on staging
+
+desc "Returning staging database on staging"
 
 task :load_tfdso_staging do
   Dir.chdir('User/Chris/Work/think_feel_do_so') do
-    system('cap staging deploy:use_staging_db')
+    system( "cap staging deploy:use_staging_db" )
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,40 +12,40 @@ Capybara.default_wait_time = 10
 Capybara.default_driver = :sauce
 
 Sauce.config do |config|
+
   config[:start_tunnel] = false
 
-  # Careful with the Platforms below
-  # IE8 on XP - this test suite does not work on this because the navbar shows
-  # up as the mobile view navbar making all navbar links referenced in the test
-  # invisible. For now this will stay commented out until a solution is found.
-  # IE10 on Windows 7 - random false negatives occur from error pages popping
-  # up. For now this will stay commented out until a solution is found.
+  #Careful with the Platforms below
+  #IE8 on XP - this test suite does not work on this because the navbar shows up as the mobile view navbar making all
+  # navbar links referenced in the test invisible. For now this will stay commented out until a solution is found.
+  #IE10 on Windows 7 - random false negatives occur from error pages popping up. For now this will stay commented out
+  # until a solution is found.
 
   config[:browsers] = [
-    # ['Windows XP', 'Internet Explorer', '8'],
-    ['Windows XP', 'Firefox', '32'],
-    ['Windows XP', 'Chrome', '37'],
-    # ['Windows 7', 'Internet Explorer', '10'],
-    ['Windows 7', 'Firefox', '32'],
-    ['Windows 7', 'Chrome', '37'],
-    ['OS X 10.6', 'Firefox', '32'],
-    ['OS X 10.6', 'Chrome', '37'],
-    ['OS X 10.6', 'Safari', '5'],
-    ['OS X 10.6', 'Chrome', '37'],
-    ['OS X 10.8', 'Safari', '6'],
-    ['OS X 10.9', 'Firefox', '32'],
-    ['OS X 10.9', 'Chrome', '37'],
-    ['OS X 10.9', 'Safari', '7'],
-    ['OS X 10.10', 'Firefox', '32'],
-    ['OS X 10.10', 'Chrome', '37']
+    # ["Windows XP", "Internet Explorer", "8"],
+    ["Windows XP", "Firefox", "32"],
+    ["Windows XP", "Chrome", "37"],
+    # ["Windows 7", "Internet Explorer", "10"],
+    ["Windows 7", "Firefox", "32"],
+    ["Windows 7", "Chrome", "37"],
+    ["OS X 10.6", "Firefox", "32"],
+    ["OS X 10.6", "Chrome", "37"],
+    ["OS X 10.6", "Safari", "5"],
+    ["OS X 10.6", "Chrome", "37"],
+    ["OS X 10.8", "Safari", "6"],
+    ["OS X 10.9", "Firefox", "32"],
+    ["OS X 10.9", "Chrome", "37"],
+    ["OS X 10.9", "Safari", "7"],
+    ["OS X 10.10", "Firefox", "32"],
+    ["OS X 10.10", "Chrome", "37"]
   ]
 
   config.after(:each) do |example|
-    if example.exception.nil?
-      SauceWhisk::Jobs.pass_job @driver.session_id
-    else
-      SauceWhisk::Jobs.fail_job @driver.session_id
-    end
+      if example.exception.nil?
+        SauceWhisk::Jobs.pass_job @driver.session_id
+      else
+        SauceWhisk::Jobs.fail_job @driver.session_id
+      end
     @driver.quit
   end
 end
