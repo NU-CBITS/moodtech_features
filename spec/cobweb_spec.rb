18c48c53126dd211e6b274eca9d26e5b7849577d
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-02-06 12:30:57 -0600

    updated cobweb. not entirely useful as I cannot get past authentication

--- spec/cobweb_spec.rb
+++ spec/cobweb_spec.rb
@@ -6,11 +6,10 @@ require '/Users/Chris/.rvm/gems/ruby-2.2.0/gems/cobweb-1.0.25/lib/cobweb.rb'
 require 'redis'
 
 describe 'cobweb', type: :feature do
-  it '- crawls all' do
-    puts 'test'
-    statistics = CobwebCrawler.new(cache: 600, redis_options: { host: 'localhost', port: '3000' }).crawl(ENV['Base_URL']) do |page|
+  it '- crawls without login' do
+    statistics = CobwebCrawler.new(cache: 600).crawl(ENV['Base_URL']) do |page|
       puts "Just crawled #{page[:url]} and got a status of #{page[:status_code]}."
-    end
-    puts "Finished Crawl with #{statistics[:page_count]} pages and #{statistics[:asset_count]} assets."
+    end.get_statistics
+    puts "Finished Crawl without login with #{statistics[:page_count]} pages and #{statistics[:asset_count]} assets."
   end
 end
