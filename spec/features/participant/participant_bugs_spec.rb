1fd8ebc06793eb619d2aa4ba75a17596ef8a48e7
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-02-23 12:08:02 -0600

    Update bug specs

--- spec/features/participant/participant_bugs_spec.rb
+++ spec/features/participant/participant_bugs_spec.rb
@@ -1,11 +1,15 @@
 # filename: participant_bugs_spec.rb
 
-# this file is to test bug fixes
-
 require_relative '../../../spec/spec_helper'
 require_relative '../../../spec/configure_cloud'
 
-describe 'Participant Bugs', type: :feature, sauce: sauce_labs do
+# define methods for this spec file
+def choose_rating(element_id, value)
+  find("##{ element_id } select").find(:xpath, "option[#{(value + 1)}]").select_option
+end
+
+# tests
+describe 'Active participant is signed in,', type: :feature, sauce: sauce_labs do
   before(:each) do
     visit ENV['Base_URL'] + '/participants/sign_in'
     within('#new_participant') do
@@ -14,14 +18,12 @@ describe 'Participant Bugs', type: :feature, sauce: sauce_labs do
     end
     click_on 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-  end
 
-  # tests
-  # Testing bug where I am receiving multiple alerts in the Planning form.
-  it '- planning bug where there are multiple alerts' do
     visit ENV['Base_URL'] + '/navigator/contexts/DO'
     expect(page).to have_content 'Add a New Activity'
+  end
 
+  it 'completes DO > Planning without receiving multiple alerts' do
     click_on '#2 Planning'
     expect(page).to have_content 'The last few times you were here...'
 
@@ -59,11 +61,7 @@ describe 'Participant Bugs', type: :feature, sauce: sauce_labs do
     expect(page).to have_content 'Upcoming Activities'
   end
 
-  # Testing bug where I am receiving multiple alerts in the Plan a New Activity Form
-  it '- plan a new activity' do
-    visit ENV['Base_URL'] + '/navigator/contexts/DO'
-    expect(page).to have_content 'Add a New Activity'
-
+  it 'completes DO > Plan a New Activity without multiple alerts' do
     click_on 'Add a New Activity'
     expect(page).to have_content "But you don't have to start from scratch,"
 
@@ -80,40 +78,33 @@ describe 'Participant Bugs', type: :feature, sauce: sauce_labs do
     expect(page).to have_content 'Add a New Activity'
   end
 
-  # Testing bug where expection is thrown when clicking on Previous Day in Your Activities
-  it 'visits your activities and selects Previous Day' do
+  it 'visits your activities and selects Previous Day without receiving exception' do
     click_on '#1 Awareness'
     expect(page).to have_content 'This is just the beginning...'
 
     click_on 'Next'
     expect(page).to have_content "OK, let's talk about yesterday."
 
-    within('awake_period[start_time]') do
-      today = Date.today
-      yesterday = Date.today.prev_day
-      if page.has_text?(yesterday.strftime('%a') + ' 4 AM')
-        select yesterday yesterday.strftime('%a') + ' 4 AM', from: 'awake_period_start_time'
-      else
-        select today.strftime('%a') + ' 12 AM', from: 'awake_period_start_time'
-      end
+    today = Date.today
+    yesterday = Date.today.prev_day
+    if page.has_text?(yesterday.strftime('%a') + ' 4 AM')
+      select yesterday.strftime('%a') + ' 4 AM', from: 'awake_period_start_time'
+    else
+      select today.strftime('%a') + ' 12 AM', from: 'awake_period_start_time'
     end
 
-    within('awake_period[end_time]') do
-      today = Date.today
-      yesterday = Date.today.prev_day
-      if page.has_text?(yesterday.strftime('%a') + ' 5 AM')
-        select yesterday yesterday.strftime('%a') + ' 5 AM', from: 'awake_period_end_time'
-      else
-        select today.strftime('%a') + ' 1 AM', from: 'awake_period_end_time'
-      end
+    if page.has_text?(yesterday.strftime('%a') + ' 5 AM')
+      select yesterday.strftime('%a') + ' 5 AM', from: 'awake_period_end_time'
+    else
+      select today.strftime('%a') + ' 1 AM', from: 'awake_period_end_time'
     end
 
     click_on 'Create'
     expect(page).to have_content 'Awake Period saved'
 
     fill_in 'activity_type_0', with: 'Sleep'
-    choose_rating('pleasure_9', 6)
-    choose_rating('accomplishment_3', 1)
+    choose_rating('pleasure_0', 9)
+    choose_rating('accomplishment_0', 3)
     click_on 'Next'
     page.accept_alert 'Are you sure that you would like to make these public?'
     expect(page).to have_content 'Activity saved'
@@ -135,5 +126,47 @@ describe 'Participant Bugs', type: :feature, sauce: sauce_labs do
     click_on 'Previous Day'
     yesterday = today - 1
     expect(page).to have_content 'Daily Averages for ' + yesterday.strftime('%b %d, %Y')
+  end
+
+  it 'completes DO > Awareness and finds the activity properly displayed on feed' do
+    click_on '#1 Awareness'
+    expect(page).to have_content 'This is just the beginning...'
+
+    click_on 'Next'
+    expect(page).to have_content "OK, let's talk about yesterday."
+
+    today = Date.today
+    select today.strftime('%a') + ' 1 AM', from: 'awake_period_start_time'
+    select today.strftime('%a') + ' 2 AM', from: 'awake_period_end_time'
+    click_on 'Create'
+    expect(page).to have_content 'Awake Period saved'
+
+    fill_in 'activity_type_0', with: 'doing whatever thing'
+    choose_rating('pleasure_0', 6)
+    choose_rating('accomplishment_0', 7)
+    click_on 'Next'
+    page.accept_alert 'Are you sure that you would like to make these public?'
+    expect(page).to have_content 'Activity saved'
+
+    expect(page).to have_content 'Take a look - does this all seem right? Recently, you...'
+
+    click_on 'Next'
+    expect(page).to have_content 'Things you found fun.'
+
+    click_on 'Next'
+    expect(page).to have_content "Things that make you feel like you've accomplished something."
+
+    click_on 'Next'
+    expect(page).to have_content 'Add a New Activity'
+
+    visit ENV['Base_URL']
+    within('.list-group-item.ng-scope', text: 'monitored an Activity: doing whatever thing') do
+      within('.actions') do
+        find('.fa.fa-folder-open.fa-2x.ng-scope').click
+      end
+      expect(page).to have_content 'actual accomplishment: 7'
+
+      expect(page).to have_content 'actual pleasure: 6'
+    end
   end
 end
