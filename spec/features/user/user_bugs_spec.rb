adf074665d6c944016ca4e077112fe969966776d
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-03-25 09:29:18 -0500

    Add bug example for Tool Use table data

--- spec/features/user/user_bugs_spec.rb
+++ spec/features/user/user_bugs_spec.rb
@@ -1,64 +1,98 @@
 # filename: user_bugs_spec.rb
 
-describe 'User Dashboard Bugs, Researcher signs in,',
-         type: :feature, sauce: sauce_labs do
-  before do
-    sign_in_user(ENV['Researcher_Email'], ENV['Researcher_Password'])
+describe 'User Dashboard Bugs,', type: :feature, sauce: sauce_labs do
+  context 'Researcher signs in,' do
+    before do
+      sign_in_user(ENV['Researcher_Email'], ENV['Researcher_Password'])
+    end
+
+    it 'creates a participant, assigns a group membership, sees correct ' \
+       'calculation of end date' do
+      click_on 'Participants'
+      click_on 'New'
+      fill_in 'participant_study_id', with: 'Tests'
+      fill_in 'participant_email', with: 'test@test.com'
+      fill_in 'participant_phone_number', with: ENV['Participant_Phone_Number']
+      select 'Email', from: 'participant_contact_preference'
+      click_on 'Create'
+      expect(page).to have_content 'Participant was successfully created.'
+
+      click_on 'Assign New Group'
+      select 'Group 1', from: 'membership_group_id'
+      fill_in 'membership_display_name', with: 'Tester'
+      fill_in 'membership_start_date',
+              with: Date.today.prev_day.strftime('%Y-%m-%d')
+      next_year = Date.today + 365
+      fill_in 'membership_end_date', with: next_year.strftime('%Y-%m-%d')
+      weeks_later = Date.today + 56
+      expect(page).to have_content 'Standard number of weeks: 8, Projected End ' \
+                                   'Date from today: ' \
+                                   "#{weeks_later.strftime('%-m/%-d/%Y')}"
+
+      click_on 'Assign'
+      expect(page).to have_content 'Group was successfully assigned'
+
+      expect(page).to have_content "Membership Status: Active\nCurrent Group: " \
+                                   'Group 1'
+    end
+
+    it 'creates a participant, assigns a social group membership without a ' \
+       'display name, receives alert that display name is needed' do
+      click_on 'Participants'
+      click_on 'New'
+      fill_in 'participant_study_id', with: 'Fake'
+      fill_in 'participant_email', with: 'fake@test.com'
+      select 'Email', from: 'participant_contact_preference'
+      click_on 'Create'
+      expect(page).to have_content 'Participant was successfully created.'
+
+      click_on 'Assign New Group'
+      select 'Group 6', from: 'membership_group_id'
+      fill_in 'membership_start_date',
+              with: Date.today.prev_day.strftime('%Y-%m-%d')
+      next_year = Date.today + 365
+      fill_in 'membership_end_date', with: next_year.strftime('%Y-%m-%d')
+
+      click_on 'Assign'
+      expect(page).to have_content 'Group 6 is part of a social arm. ' \
+                                   'Display name is required for social arms.'
+
+      within('#membership_group_id') do
+        expect(page).to have_content 'Group 6'
+      end
+    end
   end
 
-  it 'creates a participant, assigns a group membership, sees correct ' \
-     'calculation of end date' do
-    click_on 'Participants'
-    click_on 'New'
-    fill_in 'participant_study_id', with: 'Tests'
-    fill_in 'participant_email', with: 'test@test.com'
-    fill_in 'participant_phone_number', with: ENV['Participant_Phone_Number']
-    select 'Email', from: 'participant_contact_preference'
-    click_on 'Create'
-    expect(page).to have_content 'Participant was successfully created.'
+  context 'Clinician signs in,' do
+    before do
+      sign_in_user(ENV['Researcher_Email'], ENV['Researcher_Password'])
+    end
 
-    click_on 'Assign New Group'
-    select 'Group 1', from: 'membership_group_id'
-    fill_in 'membership_display_name', with: 'Tester'
-    fill_in 'membership_start_date',
-            with: Date.today.prev_day.strftime('%Y-%m-%d')
-    next_year = Date.today + 365
-    fill_in 'membership_end_date', with: next_year.strftime('%Y-%m-%d')
-    weeks_later = Date.today + 56
-    expect(page).to have_content 'Standard number of weeks: 8, Projected End ' \
-                                 'Date from today: ' \
-                                 "#{weeks_later.strftime('%-m/%-d/%Y')}"
+    it 'navigates to Patient Dashboard, views Tool Use table, sees correct ' \
+     'data for activities'do
+      click_on 'Arms'
+      find('h1', text: 'Arms')
+      click_on 'Arm 1'
+      click_on 'Group 1'
+      click_on 'Patient Dashboard'
+      select_patient('TFD-1111')
+      within('.table.table-hover', text: 'Tool Use') do
+        within('tr', text: 'Activities Planned') do
+          expect(page).to have_content '2 2 2'
+        end
 
-    click_on 'Assign'
-    expect(page).to have_content 'Group was successfully assigned'
+        within('tr', text: 'Activities Monitored') do
+          expect(page).to have_content '4 20 20'
+        end
 
-    expect(page).to have_content "Membership Status: Active\nCurrent Group: " \
-                                 'Group 1'
-  end
+        within('tr', text: 'Activities Reviewed and Completed') do
+          expect(page).to have_content '1 2 2'
+        end
 
-  it 'creates a participant, assigns a social group membership without a ' \
-     'display name, receives alert that display name is needed' do
-    click_on 'Participants'
-    click_on 'New'
-    fill_in 'participant_study_id', with: 'Fake'
-    fill_in 'participant_email', with: 'fake@test.com'
-    select 'Email', from: 'participant_contact_preference'
-    click_on 'Create'
-    expect(page).to have_content 'Participant was successfully created.'
-
-    click_on 'Assign New Group'
-    select 'Group 6', from: 'membership_group_id'
-    fill_in 'membership_start_date',
-            with: Date.today.prev_day.strftime('%Y-%m-%d')
-    next_year = Date.today + 365
-    fill_in 'membership_end_date', with: next_year.strftime('%Y-%m-%d')
-
-    click_on 'Assign'
-    expect(page).to have_content 'Group 6 is part of a social arm. ' \
-                                 'Display name is required for social arms.'
-
-    within('#membership_group_id') do
-      expect(page).to have_content 'Group 6'
+        within('tr', text: 'Activities Reviewed and Incomplete') do
+          expect(page).to have_content '1 1 1'
+        end
+      end
     end
   end
 end
