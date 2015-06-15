78487400a33bedda8155d021480e45a917aa9ee7
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-06-15 15:54:46 -0500

    Update login example in user_bugs_spec.rb

--- spec/features/user/user_bugs_spec.rb
+++ spec/features/user/user_bugs_spec.rb
@@ -77,7 +77,7 @@ describe 'User Dashboard Bugs,', type: :feature, sauce: sauce_labs do
       click_on 'Patient Dashboard'
       within('#patients') do
         within('table#patients tr', text: 'participant61') do
-          expect(page).to have_content 'participant61 0 6 11'
+          expect(page).to have_content 'participant61 0 6 12'
         end
       end
 
@@ -85,10 +85,10 @@ describe 'User Dashboard Bugs,', type: :feature, sauce: sauce_labs do
       within('.panel.panel-default', text: 'Login Info') do
         date1 = Date.today - 4
         expect(page).to have_content 'Last Logged In: ' \
-                                     "#{date1.strftime('%b %d %Y %H')}"
+                                     "#{date1.strftime('%b %d %Y')}"
 
         expect(page).to have_content "Logins Today: 0\nLogins during this " \
-                                     "treatment week: 0\nTotal Logins: 11"
+                                     "treatment week: 0\nTotal Logins: 12"
       end
     end
 
